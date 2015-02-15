d4ef2ebbf42b9d48e1ae86f5e7942f6d445f27a9
nocs13 <nocs13@gmail.com>
2015-02-15 21:37:22 +0400

    ...

--- src/com/kgm/kNotes/Main.java
+++ src/com/kgm/kNotes/Main.java
@@ -1,11 +1,12 @@
 package com.kgm.kNotes;
 
-import com.kgm.knotes.R;
+import com.kgm.kNotes.R;
 
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.view.View.OnClickListener;
 
 public class Main extends Activity {
 	public Main() {
@@ -16,5 +17,25 @@ public class Main extends Activity {
 		super.onCreate(savedInstanceState);
 
 		setContentView(R.layout.lay_main);
+		
+		Button btn = (Button) findViewById(R.id.btn_new);
+		
+		if (btn != null) {
+			btn.setOnClickListener(new OnClickListener() {
+				public void onClick(View v) {
+					setContentView(R.layout.lay_new);
+
+					Button btn = (Button) findViewById(R.id.btn_add);
+					
+					if (btn != null) {
+						btn.setOnClickListener(new OnClickListener() {
+							public void onClick(View v) {
+								
+							}
+						});
+					}
+				}
+			});
+		}
 	}
 }
---
+++ src/com/kgm/kNotes/Note.java
@@ -0,0 +1,30 @@
+package com.kgm.kNotes;
+
+public class Note {
+
+	private String title = "";
+	private String note  = "";
+	private String date  = "";
+	
+	public Note() {
+		
+	}
+	
+	public Note(String title, String note, String date) {
+		this.title = title;
+		this.note  = note;
+		this.date  = date;
+	}
+	
+	public String getTitle() {
+		return title;
+	} 
+	
+	public String getNote() {
+		return note;
+	}
+	
+	public String getDate() {
+		return date;
+	}
+}
